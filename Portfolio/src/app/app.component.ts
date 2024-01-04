0b918c41ffe6873b7cbc7029f9f09cc5851b4f10
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-04 19:51:36 +0100

    component Skills

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './landing-page/navbar/navbar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent,
+            SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
