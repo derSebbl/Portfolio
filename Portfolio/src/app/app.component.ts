5917ed8df5f555eac9e7dba3cb3d01159518e14b
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-05 17:35:51 +0100

    component left

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './landing-page/navbar/navbar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { WorkLeftComponent } from './work-left/work-left.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent,
-            SkillsComponent],
+            SkillsComponent, WorkLeftComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Portfolio/src/app/work-left/work-left.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-left',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-left.component.html',
+  styleUrl: './work-left.component.scss'
+})
+export class WorkLeftComponent {
+
+}
