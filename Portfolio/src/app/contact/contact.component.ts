63ad17a446e649ff7a8f7483379a1d523b8d79de
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-07 15:15:09 +0100

    add scroll back to top

--- Portfolio/src/app/contact/contact.component.ts
+++ Portfolio/src/app/contact/contact.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
