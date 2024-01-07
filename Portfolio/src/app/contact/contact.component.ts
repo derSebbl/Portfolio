f18a46de9baa90c99700b8bf9eea300498544a02
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-07 15:39:33 +0100

    add formsModule

--- Portfolio/src/app/contact/contact.component.ts
+++ Portfolio/src/app/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
