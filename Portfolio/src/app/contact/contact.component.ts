b02f41ec4b7b3db1504564648f8492a6518c0524
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-12 21:08:44 +0100

    fix error on inputs where Placeholder slides back in

--- Portfolio/src/app/contact/contact.component.ts
+++ Portfolio/src/app/contact/contact.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  name:string = '';
+  email:string = '';
+  message:string = '';
+
+
+  ngOnInit() {
+    this.checkInputValues();
+  }
+  checkInputValues() {
+    let inputs = document.querySelectorAll('.nameInput input, .mailInput input, .textInput textarea');
+    inputs.forEach((inputElement) => {
+      if (inputElement instanceof HTMLInputElement || inputElement instanceof HTMLTextAreaElement) {
+        inputElement.addEventListener('input', () => {
+          if (inputElement.value.trim() === '') {
+            inputElement.parentElement?.classList.remove('not-empty');
+          } else {
+            inputElement.parentElement?.classList.add('not-empty');
+          }
+        });
+      }
+    });
+  }
 
   scrollToTop() {
     window.scrollTo({
