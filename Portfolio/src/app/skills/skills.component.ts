538135ee48352edd6fb88b5f2ddf7b8fff806b01
Sebastian <139996363+derSebbl@users.noreply.github.com>
2024-01-20 14:16:45 +0100

    edit skills responisv

--- Portfolio/src/app/skills/skills.component.ts
+++ Portfolio/src/app/skills/skills.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const target = event.target as Window;
+    const htmlElement = this.el.nativeElement.querySelector('#html');
+    const scrumElement = this.el.nativeElement.querySelector('#scrum');
+    const skillRowBottomElement =
+      this.el.nativeElement.querySelector('.skillRowBottom');
+    const skillRowTopElement =
+      this.el.nativeElement.querySelector('.skillRowTop');
+    if (target.innerWidth < 645) {
+      if (htmlElement && scrumElement && skillRowBottomElement) {
+        this.renderer.appendChild(skillRowBottomElement, scrumElement);
+      }
+    }
+    if (target.innerWidth > 645) {
+      if (htmlElement && scrumElement && skillRowBottomElement) {
+        if (skillRowBottomElement.contains(scrumElement)) {
+          this.renderer.appendChild(skillRowTopElement, scrumElement);
+        }
+      }
+    }
+    if (target.innerWidth < 534) {
+      if (htmlElement && scrumElement && skillRowBottomElement) {
+        this.renderer.appendChild(skillRowBottomElement, htmlElement);
+      }
+    }
+    if (target.innerWidth > 534) {
+      if (htmlElement && scrumElement && skillRowBottomElement) {
+        if (skillRowBottomElement.contains(htmlElement)) {
+          this.renderer.appendChild(skillRowTopElement, htmlElement);
+        }
+      }
+    }
+  }
 }
---
+++ Portfolio/src/app/skills/skills.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SkillsComponent } from '../skills/skills.component';
+
+@NgModule({
+    imports: [CommonModule, SkillsComponent]
+})
+export class SkillsModule { }
